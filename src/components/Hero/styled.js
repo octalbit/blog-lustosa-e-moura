4f7276d8e89873cdf6e5d45c252512281a23fd2f
jacksonbrunoss <oi.jbss@gmail.com>
2019-10-01 23:58:00 -0300

    Styleds component Hero

--- src/components/Hero/styled.js
+++ src/components/Hero/styled.js
@@ -1,7 +1,43 @@
 import styled from 'styled-components'
 
+import BgHero from '../../images/bg-hero.jpg'
+
 export const HeroWrapper = styled.section`
     width: 100%;
-    height: 700px;
-    background-color: #7777;
+    height: 600px;
+    background-image: url(${BgHero});
+    background-position: center;
+    background-size: cover;
+`
+export const HeroContainer = styled.div`
+    max-width: 1000px;
+    margin: auto;
+    height: 100%;
+`
+export const HeroContent = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const HeroBannerBox = styled.div`
+    max-width: 700px;
+`
+export const HeroBannerColor = styled.span`
+    color: #B88A34;
+`
+export const HeroBannerTitle = styled.h1`
+    font-size: 60px;
+    font-weight: 800;
+    text-transform: uppercase;
+    text-align: center;
+    color: #fff;
+    padding: 0px 0px 30px 0px;
+`
+export const HeroBannerSubtitle = styled.p`
+    font-size: 1.0rem;
+    text-align: center;
+    color: #eee;
+    line-height: 1.3rem;
 `
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-//import Hero from '../components/Hero'
+import Hero from '../components/Hero'
 //import About from '../components/About'
 //import Area from '../components/Area'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
+    <Hero />
   </Layout>
 )
 
