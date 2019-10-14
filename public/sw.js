1f509236a8e29746721b4dfd3598a473a19d8b21
Rafael Souza <rafaelangical2@gmail.com>
2019-10-14 17:29:04 -0300

    Rename Icons to icons

---
+++ public/sw.js
@@ -0,0 +1,132 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-e443d64ff19c3d8b5a4c.js"
+  },
+  {
+    "url": "commons-3d60d4c1b70f3c5e3890.js"
+  },
+  {
+    "url": "styles.5f81339daa9828444137.css"
+  },
+  {
+    "url": "styles-2c7d4ecbb2ee1ae0a2ce.js"
+  },
+  {
+    "url": "app-aa091ac61458604d42b6.js"
+  },
+  {
+    "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-98c64b6c3e01e1765829.js"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "be51f079a437abbb0c0dbd405b989b60"
+  },
+  {
+    "url": "manifest.webmanifest",
+    "revision": "9657775b68862db0f366cfe11943fa70"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, new workbox.strategies.CacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, new workbox.strategies.NetworkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, new workbox.strategies.StaleWhileRevalidate(), 'GET');
+
+/* global importScripts, workbox, idbKeyval */
+
+importScripts(`idb-keyval-iife.min.js`)
+
+const { NavigationRoute } = workbox.routing
+let offlineShellEnabled = true
+
+const navigationRoute = new NavigationRoute(async ({ event }) => {
+  if (!offlineShellEnabled) {
+    return await fetch(event.request)
+  }
+
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
+
+  // Check for resources + the app bundle
+  // The latter may not exist if the SW is updating to a new version
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-aa091ac61458604d42b6.js`))) {
+    return await fetch(event.request)
+  }
+
+  for (const resource of resources) {
+    // As soon as we detect a failed resource, fetch the entire page from
+    // network - that way we won't risk being in an inconsistent state with
+    // some parts of the page failing.
+    if (!(await caches.match(resource))) {
+      return await fetch(event.request)
+    }
+  }
+
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  const offlineShellWithKey = workbox.precaching.getCacheKeyForURL(offlineShell)
+  return await caches.match(offlineShellWithKey)
+})
+
+workbox.routing.registerRoute(navigationRoute)
+
+// prefer standard object syntax to support more browsers
+const MessageAPI = {
+  setPathResources: (event, { path, resources }) => {
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
+  },
+
+  clearPathResources: event => {
+    event.waitUntil(idbKeyval.clear())
+  },
+
+  enableOfflineShell: () => {
+    offlineShellEnabled = true
+  },
+
+  disableOfflineShell: () => {
+    offlineShellEnabled = false
+  },
+}
+
+self.addEventListener(`message`, event => {
+  const { gatsbyApi: api } = event.data
+  if (api) MessageAPI[api](event, event.data)
+})
+
+workbox.routing.registerRoute(/\/.gatsby-plugin-offline:.+/, ({ event }) => {
+  const { pathname } = new URL(event.request.url)
+
+  const api = pathname.match(/:(.+)/)[1]
+  MessageAPI[api]()
+
+  return new Response()
+})
--- src/components/SocialLinks/index.js
+++ src/components/SocialLinks/index.js
@@ -1,32 +1,27 @@
 import React from 'react';
 
-import Icons from "./Icons"
-import links from "./content"
+import Icons from './icons';
+import links from './content';
 import * as S from './styled';
 
 const SocialLinks = () => (
-    <S.SocialLinksWrapper>
-        <S.SocialLinksList>
-        {links.map((link, i) => {
-            const Icon = Icons[link.label]
+	<S.SocialLinksWrapper>
+		<S.SocialLinksList>
+			{links.map((link, i) => {
+				const Icon = Icons[link.label];
 
-            return (
-                <S.SocialLinksItem key={i}>
-                    <S.SocialLinksLink
-                    href={link.url}
-                    title={link.label}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    >
-                    <S.IconWrapper>
-                        <Icon />
-                    </S.IconWrapper>
-                    </S.SocialLinksLink>
-                </S.SocialLinksItem>
-        )
-      })}
-    </S.SocialLinksList>
-    </S.SocialLinksWrapper>
-)
+				return (
+					<S.SocialLinksItem key={i}>
+						<S.SocialLinksLink href={link.url} title={link.label} target="_blank" rel="noopener noreferrer">
+							<S.IconWrapper>
+								<Icon />
+							</S.IconWrapper>
+						</S.SocialLinksLink>
+					</S.SocialLinksItem>
+				);
+			})}
+		</S.SocialLinksList>
+	</S.SocialLinksWrapper>
+);
 
-export default SocialLinks
+export default SocialLinks;
