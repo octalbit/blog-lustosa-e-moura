9d784dedde129b2fa18eca910649b100b37d139b
Rafael Souza <rafaelangical2@gmail.com>
2023-06-13 14:27:30 -0300

    fix: correcao path imagem whats

--- src/components/ButtonWhatsappSocial/index.js
+++ src/components/ButtonWhatsappSocial/index.js
@@ -2,14 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import WhatsImg from "../../images/whatsapp.png"
 
-const Button = styled.img`
+const Button = styled.link`
     position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
     bottom: 10px;
     right: 10px;
 `;
 
 const ButtonWhatsappSocial = () => (
-   <link href="https://wa.link/2luout"><Button src={WhatsImg}/></link>
+   <Button href="https://wa.link/2luout">
+        <img width={60} height={60} src={WhatsImg} alt="whatsapp-icon"/>
+    </Button>
 );
 
 export default ButtonWhatsappSocial;
