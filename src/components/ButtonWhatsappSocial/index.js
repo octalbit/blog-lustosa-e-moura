f63ea1cb959b02de394e59f065ef50658515b2d5
Rafael Souza <rafaelangical2@gmail.com>
2023-06-14 02:33:46 -0300

    feat: botao whats

--- src/components/ButtonWhatsappSocial/index.js
+++ src/components/ButtonWhatsappSocial/index.js
@@ -1,21 +1,21 @@
-// import React from "react";
-// import styled from "styled-components";
-// import WhatsImg from "../../images/whatsapp.png"
+import React from "react";
+import styled from "styled-components";
+import WhatsImg from "../../images/whatsapp.png"
 
-// const Button = styled.link`
-//     position: absolute;
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     justify-content: center;
-//     bottom: 10px;
-//     right: 10px;
-// `;
+const Link = styled.a`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    bottom: 10px;
+    right: 10px;
+`;
 
-// const ButtonWhatsappSocial = () => (
-//    <Button href="https://wa.link/2luout">
-//         <img width={60} height={60} src={WhatsImg} alt="whatsapp-icon"/>
-//     </Button>
-// );
+const ButtonWhatsappSocial = () => (
+   <Link href="https://wa.link/2luout">
+        <img width={60} height={60} src={WhatsImg} alt="whatsapp-icon"/>
+    </Link>
+);
 
-// export default ButtonWhatsappSocial;
+export default ButtonWhatsappSocial;
