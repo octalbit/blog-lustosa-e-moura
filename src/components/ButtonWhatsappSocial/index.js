69e2cfbd099a2e25c4bff8182ed911f8022b1942
Rafael Souza <rafaelangical2@gmail.com>
2023-07-12 13:05:15 -0300

    fix: correcao botao whatsapp

--- src/components/ButtonWhatsappSocial/index.js
+++ src/components/ButtonWhatsappSocial/index.js
@@ -1,4 +1,19 @@
 import React from "react"
+import styled from "styled-components"
+
+const Link = styled.a`
+    position:fixed;
+    width:60px;
+    height:60px;
+    bottom:40px;
+    right:40px;
+    background-color:#25d366;
+    color:#FFF;
+    border-radius:50px;
+    text-align:center;
+    font-size:30px;
+    box-shadow: 1px 1px 2px #888;
+`;
 
 const ButtonWhatsappSocial = () => (
   <div>
@@ -6,26 +21,12 @@ const ButtonWhatsappSocial = () => (
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"
     />
-    <a
+    <Link
       href="https://wa.link/2luout"
-      style={{
-        position: "fixed",
-        width: 60,
-        height: 60,
-        bottom: 40,
-        right: 40,
-        backgroundColor: "#25d366",
-        color: "#FFF",
-        borderRadius: 50,
-        textAlign: "center",
-        fontSize: 30,
-        boxShadow: "1px 1px 2px #888",
-        zIndex: 1000,
-      }}
       target="_blank"
     >
-      <i style="margin-top:16px" class="fa fa-whatsapp"></i>
-    </a>
+      <i style={{ marginTop: 16 }} className="fa fa-whatsapp"></i>
+    </Link>
   </div>
 )
 
