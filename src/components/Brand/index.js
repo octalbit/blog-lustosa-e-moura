aa9fddc7b045563fd19517083b66d0aa481f2f73
Rafael Souza <rafaelangical2@gmail.com>
2023-07-13 20:49:20 -0300

    feat: atualiza logo

--- src/components/Brand/index.js
+++ src/components/Brand/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 import * as S from "./styled"
-import BrandImg from "../../images/logo-dourada.png"
+import BrandImg from "../../images/logo.png"
 
 const Brand = () => (
   <S.BrandWrapper to="/">
