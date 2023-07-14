c4d7a81739a3c1557b079e00f97188d01efab433
Rafael Souza <rafaelangical2@gmail.com>
2023-07-13 21:05:18 -0300

    feat: atualiza favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,7 +74,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/lustosa-icon.png`,
+        icon: `content/assets/logo-dourada.png`,
       },
     },
     `gatsby-plugin-offline`,
